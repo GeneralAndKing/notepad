8fba0a120e62679d5ea43bbca42ea2bc172ef11b
fengxin <930926134@qq.com>
2020-06-09 01:52:33 +0800

    完成置顶（存在一堆bug） 完成item换肤

--- app/src/main/java/com/np/notepad/model/NoteItem.kt
+++ app/src/main/java/com/np/notepad/model/NoteItem.kt
@@ -1,7 +1,6 @@
 package com.np.notepad.model
 
-import android.graphics.Color
-import com.np.notepad.model.enums.NoteTypeEnum
+import com.np.notepad.model.enums.BackgroundTypeEnum
 import org.litepal.crud.LitePalSupport
 import java.util.*
 
@@ -13,12 +12,11 @@ data class NoteItem (
     var id: Long = 0,
     var title: String = "",
     var content: String = "",
-    var backgroundColor: Int = Color.WHITE,
     /**置顶*/
     var top: Boolean = false,
     /**强提醒*/
     var remind: Boolean = false,
     /**笔记类别*/
-    var noteType: NoteTypeEnum = NoteTypeEnum.DEFAULT,
+    var background: BackgroundTypeEnum = BackgroundTypeEnum.GREEN,
     var lastUpdateTime: Date = Date()
 ): LitePalSupport()
---
+++ app/src/main/java/com/np/notepad/model/enums/BackgroundTypeEnum.kt
@@ -0,0 +1,29 @@
+package com.np.notepad.model.enums
+
+import com.np.notepad.R
+
+
+/**
+ * 笔记背景类型
+ * @author z f
+ */
+enum class BackgroundTypeEnum(val resId: Int, val des: String) {
+    RED(R.drawable.item_red_background,"红色"),
+    YELLOW(R.drawable.item_yellow_background,"黄色"),
+    CYAN(R.drawable.item_cyan_background,"青色"),
+    BLUE(R.drawable.item_blue_background,"蓝色"),
+    GREEN(R.drawable.item_green_background,"绿色");
+
+    companion object {
+        /**
+         * 获取DesArray
+         */
+        fun getDesArray(): Array<String> {
+            val desArray: Array<String> = Array(values().size) {""}
+            for ((i, noteType) in values().withIndex()) {
+                desArray[i] = noteType.des
+            }
+            return desArray
+        }
+    }
+}
